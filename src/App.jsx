2a3a2e16349a3edd2617236085f776bc7b2fb0eb
oOValantisOo <jakendra.lathaniel@gmail.com>
2026-03-09 08:27:52 +0700

    styling misplace fix

--- src/App.jsx
+++ src/App.jsx
@@ -195,10 +195,10 @@ function App() {
     <div className="education">
       <h1 className="text-4xl text-black dark:text-gray-900">EDUCATION</h1>
       <div className="school flex items-stretch pt-5" id="school">
-        <div className="leftSchool bg-white dark:bg-white text-black dark:text-black">
+        <div className="leftSchool">
           <img src={schoolgrad} className="h-full object-cover" alt="" />
         </div>
-        <div className="rightSchool p-5">
+        <div className="rightSchool p-5 bg-white dark:bg-white text-black dark:text-black">
           <h2 className="text-xl md:text-2xl lg:text-3xl font-semibold">Notre Dame Senior High School</h2>
           <h3 className="text-lg md:text-xl lg:text-2xl text-gray-500">Jakarta, Indonesia</h3>
           <h5 className="mt-2">Graduated as a high-ranking student, excelling through achievements of the highest grade in mathematics and english.</h5>
